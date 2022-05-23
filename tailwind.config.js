db952366fd6616380a94d15ca8276b780640797a
Agustin Beitia <abeitia@uoc.edu>
2022-05-22 19:31:02 -0500

    Colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-    colors: {'chokolate': '#8a3324', 'cyan-600': '#fefe33'
+    colors: {'chokolate': '#8a3324', 'cyan-600': '#fefe33', 'verde': '#84ff84', 'violeta':'#6b1882'
              
             },
 fontFamily : {
