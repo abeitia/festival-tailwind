c1e3c6b733dd8c93c1e517d996686b4338d49872
Agustin Beitia <abeitia@uoc.edu>
2022-06-02 11:26:38 -0500

    ajustes forma contacto

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
-    colors: {'warning':'#ffc107', 'dark':'#212529', 'cyan-600': '#fefe33','secondary':'#6c757d', 'light':'#f8f9fa', 'verde': '#84ff84', 'violeta':'#6b1882', 'accent':'#f3321f'
+    colors: {'warning':'#ffc107', 'dark':'#212529', 'cyan-600': '#fefe33','secondary':'#6c757d', 'light':'#f8f9fa', 'verde': '#84ff84', 'primary':'#6b1882', 'accent':'#f3321f'
              
             },
 fontFamily : {
