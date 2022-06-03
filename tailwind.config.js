aefdbffa1c15c6581b61a879e3787b2616863031
Agustin Beitia <abeitia@uoc.edu>
2022-06-03 12:32:43 -0500

    clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-    colors: {'warning':'#ffc107', 'dark':'#212529','secondary':'#6c757d', 'light':'#f8f9fa', 'brand': '#84ff84', 'primary':'#6b1882', 'accent':'#f3321f'
-             
-            },
-fontFamily : {
-  'serif'    : ['Merriweather', ...defaultTheme.fontFamily.serif ],
-  'sans'    : ['"Open Sans"', ...defaultTheme.fontFamily.sans ],
-
-}
-}, 
+      colors: {
+        warning: "#ffc107",
+        dark: "#212529",
+        secondary: "#6c757d",
+        light: "#f8f9fa",
+        brand: "#84ff84",
+        primary: "#6b1882",
+        accent: "#f3321f",
+      },
+      fontFamily: {
+        serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [require('@tailwindcss/forms')],
-}
-
-
-
+  plugins: [require("@tailwindcss/forms")],
+};
